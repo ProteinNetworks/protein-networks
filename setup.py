9d6ec9f3c8e107219d50e6293a129e8c4a14a0cb
Will G <wpg23@cam.ac.uk>
2017-03-24 23:10:23 +0000

    correct version information

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='proteinnetworks',
-      version='0.1',
+      version='0.1dev',
       description='Network analysis tools for protein structure',
       url='http://github.com/turingcompliant/protein-networks',
       author='William Grant',
