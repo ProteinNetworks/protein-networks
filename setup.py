6c86ba6cf7b670e9577d8d5d1f5752569154c673
Will G <wpg23@cam.ac.uk>
2017-03-24 23:07:07 +0000

    add stub init and setup files for proteinnetwork package
    Python needs a __init__.py to recognise proteinnetworks as a module
    that can be imported, and the package requires a setup.py so that
    it can be pip installed. See Kenneth Reitz for an example package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='proteinnetworks',
+      version='0.1',
+      description='Network analysis tools for protein structure',
+      url='http://github.com/turingcompliant/protein-networks',
+      author='William Grant',
+      author_email='contact@wpg.io',
+      license='GPLv3',
+      packages=['proteinnetworks'],
+      zip_safe=False)
