27d813521e3c4c194742c47dbeb5f599e00fbee7
Will Grant <wpg23@cam.ac.uk>
2017-03-26 14:56:51 +0100

    Start interface to MongoDB and rename submodules

---
+++ proteinnetworks/database/database.py
@@ -0,0 +1,57 @@
+"""Database interface methods (perhaps should be a class).
+
+MongoDB database format:
+database: proteinnetworks
+collection: proteinnetworks
+document has fields:
+    _id: bla
+    pdbref: bla
+    doctype: (edgelist | partition | pdbfile ) currently
+
+if doctype == edgelist:
+    edgelisttype: (atomic | residue)
+    hydrogenstatus: (noH | Hatoms | Hbonds) (i.e. preprocessed with nothing, HAAD, or Stride)
+    scaling: the value of the cutoff used
+    data: the actual edgelist
+
+if doctype == partition:
+    edgelistid: The _id of the edgelist used in generating the partition
+    detectionmethod: (AFG | Infomap) as it stands
+
+    if detectionmethod == AFG:
+        r : The AFG parameter used in generating the partition
+        data: The 1D array giving the partition
+
+    if detectionmethod == Infomap:
+        data: A 2D numpy array giving the data
+
+if doctype == pdbfile:
+    data: The PDBfile itself
+
+
+NB this might be too large for MongoDB to handle (>16MB)
+"""
+
+import pymongo
+from pymongo.errors import ConnectionFailure
+
+
+import glob
+import os
+import datetime
+import sys
+
+
+# Server is stored locally; if I can't find it in 1 second its not running.
+client = pymongo.MongoClient(serverSelectionTimeoutMS=1000)
+try:
+    # The ismaster command is cheap and does not require auth.
+    client.admin.command('ismaster')
+except ConnectionFailure:
+    print("Server not available:")
+    print("Log file stored in /var/log/mongod/mongod.log")
+    sys.exit(1)
+
+db = client.proteinnetworks
+collection = db.proteinnetworks
+print(collection.count())
