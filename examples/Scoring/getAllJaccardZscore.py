c147cf3dcd0194300a1aa2e3dc3ade8492a34f2b
Will Grant <wpg23@cam.ac.uk>
2017-04-07 17:36:43 +0100

    Progress on z score calculation

--- examples/Scoring/getAllJaccardZscore.py
+++ examples/Scoring/getAllJaccardZscore.py
@@ -4,12 +4,52 @@ Generate Jaccard indices for the generated partitions. Output to a file.
 Then get the z-score for each entry:
 
 z-score = ( J - mu / sigma ) i.e. the number of standard deviations away from the mean.
+
 Gives the significance.
 Plot as a histogram.
 """
 import proteinnetworks
 import numpy as np
 from bson.objectid import ObjectId
+
+
+def generateNullModel(testPartition):
+    """
+    From a given partition, generate a null model.
+
+    Here the null model has the same number of boundaries as the generated partition, but with
+    the boundaries arbitrarily placed (and the same number of communities in total.)
+    """
+    # Get the total number of communities in the partition
+    numCommunities = len(set(testPartition))
+
+    # Get the number of boundaries
+    prevI = 1
+    numBoundaries = -1  # Start from -1 to avoid counting the start as a boundary
+    for i in testPartition:
+        if i != prevI:
+            numBoundaries += 1
+        prevI = i
+
+    # Place the boundaries arbitrarily, and assign each segment to a randomly chosen community
+    print(len(testPartition))
+    newBoundaries = []
+    for i in range(numBoundaries):
+        newBoundaries.append(np.random.randint(len(testPartition)))
+    newBoundaries.sort()
+    # Fill the gaps with communities in the range 1... numComs.
+    nullModel = np.zeros(len(testPartition))
+    print(newBoundaries)
+    # for i in range(len(newBoundaries) - 1):
+    #     nullModel[newBoundaries[i]:newBoundaries[i + 1] +
+    #               1] = i % numCommunities + 1
+    # Deal with the start and end bits:
+
+    # partition.plotStripeDiagram()
+    np.set_printoptions(threshold=np.nan)
+    print(nullModel)
+    spifjasopdijf
+
 
 with open("../pdbswithstorededgelists.dat") as flines:
     pdbs = [line.strip() for line in flines]
@@ -52,6 +92,18 @@ try:
                 if jaccard > maxJaccard:
                     maxJaccard = jaccard
                     maxI = i
+
+            # Generate an arbitrary number of null models.
+            numModels = 100
+            nullJaccard = []
+            for i in range(numModels):
+                nullmodel = generateNullModel(partition.data[maxI])
+                nullJaccard.append(
+                    proteinnetworks.insight.getModifiedJaccard(pfamDomains,
+                                                               nullmodel))
+
+            print(np.mean(nullJaccard))
+
             if maxI != -1:
                 jaccards.append([pdb, maxI, maxJaccard])
         except FileNotFoundError as e:
